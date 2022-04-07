2946b60a58846d1b68602c03424eeb7bcc4da058
Phu Nguyen <phunguyen4869@gmail.com>
2022-04-07 18:01:54 +0700

    fix edit payment method show

--- resources/views/admin/users/payment/edit.blade.php
+++ resources/views/admin/users/payment/edit.blade.php
@@ -1,23 +1,38 @@
 @extends('admin.main')
 
+@section('head')
+    <style>
+        .hidden {
+            display: none;
+        }
+
+    </style>
+@endsection
+
 @section('content')
     @include('admin.alert')
     <form action="#" method="POST">
-        <h4>User: {{$user->name}}</h4>
+        <h4>User: {{ $user->name }}</h4>
 
         <div class="input-group mb-3">
             <select name="payment_method" id="payment_method" class="form-control">
-                <option value="credit_card" @if ($user->payment_method == 'credit_card') selected @endif>Thẻ tín dụng</option>
-                <option value="atm_card" @if ($user->payment_method == 'atm_card') selected @endif>Thẻ ATM nội địa</option>
-                <option value="cod" @if ($user->payment_method == 'cod') selected @endif>Thanh toán khi nhận hàng</option>
+                <option value="credit_card"
+                    @if ($user->payment_method == 'credit_card') selected @endif>Thẻ tín dụng
+                </option>
+                <option value="atm_card"
+                    @if ($user->payment_method == 'atm_card') selected @endif>Thẻ ATM nội
+                    địa</option>
+                <option value="cod"
+                    @if ($user->payment_method == 'cod') selected @endif>Thanh toán khi
+                    nhận hàng</option>
             </select>
         </div>
 
-        <div id="credit_card" @if ($user->payment_method != 'credit_card')
-            class="hidden"
-        @endif>
+        <div id="credit_card"
+            @if ($user->payment_method != 'credit_card') class="hidden" @endif>
             <div class="input-group mb-3">
-                <input type="text" name="credit_card_number" class="form-control" placeholder="Credit card number"
+                <input type="text" name="credit_card_number" class="form-control"
+                    placeholder="Credit card number"
                     value="{{ $user->credit_card_number }}">
                 <div class="input-group-append">
                     <div class="input-group-text">
@@ -27,8 +42,8 @@
             </div>
 
             <div class="input-group mb-3">
-                <input type="number" name="cvv_code" class="form-control" placeholder="CVV code"
-                    value="{{ $user->cvv_code }}">
+                <input type="number" name="cvv_code" class="form-control"
+                    placeholder="CVV code" value="{{ $user->cvv_code }}">
                 <div class="input-group-append">
                     <div class="input-group-text">
                         <span class="fas fa-lock"></span>
@@ -47,7 +62,8 @@
             </div>
 
             <div class="input-group mb-3">
-                <input type="text" name="credit_card_name" class="form-control" placeholder="Credit card name"
+                <input type="text" name="credit_card_name" class="form-control"
+                    placeholder="Credit card name"
                     value="{{ $user->credit_card_name }}">
                 <div class="input-group-append">
                     <div class="input-group-text">
@@ -57,11 +73,10 @@
             </div>
         </div>
 
-        <div id="atm_card" @if ($user->payment_method != 'atm_card')
-            class="hidden"
-        @endif>
+        <div id="atm_card" @if ($user->payment_method != 'atm_card') class="hidden" @endif>
             <div class="input-group mb-3">
-                <input type="text" name="atm_card_number" class="form-control" placeholder="ATM card number"
+                <input type="text" name="atm_card_number" class="form-control"
+                    placeholder="ATM card number"
                     value="{{ $user->atm_card_number }}">
                 <div class="input-group-append">
                     <div class="input-group-text">
@@ -71,8 +86,8 @@
             </div>
 
             <div class="input-group mb-3">
-                <input type="text" name="bank_name" class="form-control" placeholder="Bank name"
-                    value="{{ $user->bank_name }}">
+                <input type="text" name="bank_name" class="form-control"
+                    placeholder="Bank name" value="{{ $user->bank_name }}">
                 <div class="input-group-append">
                     <div class="input-group-text">
                         <span class="fas fa-university"></span>
@@ -81,7 +96,8 @@
             </div>
 
             <div class="input-group mb-3">
-                <input type="text" name="atm_card_name" class="form-control" placeholder="ATM card name"
+                <input type="text" name="atm_card_name" class="form-control"
+                    placeholder="ATM card name"
                     value="{{ $user->atm_card_name }}">
                 <div class="input-group-append">
                     <div class="input-group-text">
@@ -92,7 +108,9 @@
         </div>
 
         <div class="card-footer">
-            <button type="submit" class="btn btn-primary">Sửa Payment Method</button>
+            <button type="submit" class="btn btn-primary">Sửa Payment
+                Method</button>
         </div>
+        @csrf
     </form>
 @endsection
